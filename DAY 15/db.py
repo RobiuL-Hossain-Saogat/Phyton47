0ca85b9dd2d6beb012b2e0d64950fcaf32f59644
RobiuL-Hossain-Saogat <gboy43610@gmail.com>
2024-05-24 20:34:06 +0600

    uploading day15

---
+++ DAY 15/db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+class Database:
+    def __init__(self,db):
+        self.conn=sqlite3.connect(db)
+        self.cur=self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS part (id INTEGER PRIMARY KEY, part text, customer text, price text, company text)")
+        self.conn.commit()
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM part ")
+        rows=self.cur.fetchall()
+        return rows
+    
+    def insert(self,part,customer,price,company):
+        self.cur.execute("INSERT INTO part VALUES (NULL,?,?,?,?)",(part,customer,price,company))
+        self.conn.commit()
+
+    def update(self,id,part,customer,price,company):
+        self.cur.execute("UPDATE part SET part=?,customer=?,price=?,company=? WHERE id=?",(part,customer,price,company,id))
+        self.conn.commit()
+
+    def remove(self,id):
+        self.cur.execute("DELETE FROM part WHERE id=?",(id,))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
