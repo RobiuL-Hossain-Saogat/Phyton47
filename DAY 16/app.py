c128680225e28236118945ca360afe6bd23dbcc5
RobiuL-Hossain-Saogat <gboy43610@gmail.com>
2024-05-24 20:35:26 +0600

    uploading day16

---
+++ DAY 16/app.py
@@ -0,0 +1,33 @@
+from tkinter import *
+# from tkinter.ttk import *
+import random
+
+def calculate_love(): 
+	st='0123456789'
+	digit=2
+	temp="".join(random.sample(st,digit))
+	result.config(text=temp)
+	
+app = Tk()
+applabel=Label(app,text="Synergy Calculator????").pack(pady=10)
+
+# 1st name part
+name=StringVar()
+name_label=Label(app,text="Enter Your Name").pack(pady=5)
+name_entry=Entry(app,textvariable=name).pack(pady=5)
+
+# 2nd name part
+frnd_name=StringVar()
+frnd_name_label=Label(app,text="Enter Your Friend Name").pack(pady=5)
+frnd_name_entry=Entry(app,textvariable=frnd_name).pack(pady=5)
+
+# button widget
+button=Button(app,text="Calculate",width=12,command=calculate_love).pack()
+
+#result
+result=Label(app,text=" ")
+result.pack()
+
+app.title("Synergy Culculator????")
+app.geometry('400x240')
+app.mainloop()
